e0331d5a280a48ed92bc42527ef20aaa67a22a0f
Ivana Widjanarko <ivana.widjanarko@gmail.com>
2025-04-16 16:47:24 -0400

    local vs

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ def hello():
     return "Hello from Flask in Codespaces!"
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/two')
+def hello_two():
+    return "Hello from Flask in Codespaces! This is the second route."
